4b12f8f16138d7e1ee1662e00f79de64556508d5
Koen <koenvwel@gmail.com>
2020-06-04 13:34:04 +0200

    updated 7b & c

--- Assignments/Assignment_0.R
+++ Assignments/Assignment_0.R
@@ -5,20 +5,18 @@ func_7a <- function(x) {
 func_7b <- function(){
   vase <- c(1:100)
   sample_vase <- sample(vase, 50, replace = TRUE)
-  red <- sum(sample_vase <= 30)
-  white <- sum(sample_vase > 30)
-  print(paste0("Red balls ratio: ", red/50))
-  print(paste0("White balls ratio: ",white/50))
+  red <- sum(sample_vase <= 30)/50
+  white <- sum(sample_vase > 30)/50
+  data.frame(c(red, white), row.names = c("red","white"))
 }
 
 func_7c <- function(n = 10, mr = 30, mw = 70){
   total_balls <- mr + mw
   vase <- c(1:total_balls)
   sample_vase <- sample(vase, n, replace=TRUE)
-  red <- sum(sample_vase <= mr)
-  white <- sum(sample_vase > mr)
-  print(paste0("Red balls ratio: ", red/n))
-  print(paste0("White balls ratio: ",white/n))
+  red <- sum(sample_vase <= mr)/n
+  white <- sum(sample_vase > mr)/n
+  data.frame(c(red, white), row.names = c("red","white"))
 }
 
 func_7d <-function(count = 200, sampleSize = 40, sampleMax = 90) {
